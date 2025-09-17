207e6cca97cfaecb45c40fd9c60d145774539896
valeria <valeriaorrego1143257@correo.itm.edu.co>
2025-09-16 20:43:57 -0500

    adding projects of task

---
+++ solucio.poo03/decomposition.factors/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el número a descomponer: ");
+        int numero = int.Parse(Console.ReadLine());
+        int original = numero;
+        string factores = "";
+
+        for (int i = 2; i <= numero; i++)
+        {
+            while (numero % i == 0)
+            {
+                factores += i + " x ";
+                numero /= i;
+            }
+        }
+
+        if (factores.EndsWith(" x "))
+            factores = factores.Substring(0, factores.Length - 3);
+
+        Console.WriteLine($"{original} = {factores}");
+    }
+}
---
+++ solucio.poo03/matrix.operations/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program1 
+{
+    static void Main()
+    {
+        Console.Write("Ingrese orden de la matriz: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[n, n];
+        int suma = 0;
+        int maximo = int.MinValue;
+        int minimo = int.MaxValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matriz[i, j] = (i + 1) - j;
+                Console.Write(matriz[i, j] + "\t");
+
+                suma += matriz[i, j];
+                if (matriz[i, j] > maximo) maximo = matriz[i, j];
+                if (matriz[i, j] < minimo) minimo = matriz[i, j];
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nLa sumatoria es: " + suma);
+        Console.WriteLine("El valor máximo es: " + maximo);
+        Console.WriteLine("El valor mínimo es: " + minimo);
+    }
+}
+
+ 
